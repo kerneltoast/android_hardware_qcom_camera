1cf262d039b5783b3988751c22b0ba7e9fb9dfca
Emilian Peev <epeevs@codeaurora.org>
2013-11-20 05:18:14 -0800

    QCamera2: Adds support for offline reprocessing
    - Offline reprocessing is used for regular
      capture. During this processing the capture
      channel can be stopped and deleted on backend
      side.
    - Adds internal preview restart custom parameter
    
    CRs-Fixed: 571775
    Change-Id: I711b2648fe9c9c8ee5b09d555fcd6545215cce41

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -4475,9 +4475,9 @@ int32_t QCamera2HardwareInterface::addMetaDataChannel()
 }
 
 /*===========================================================================
- * FUNCTION   : addOnlineReprocChannel
+ * FUNCTION   : addReprocChannel
  *
- * DESCRIPTION: add a online reprocess channel that will do reprocess on frames
+ * DESCRIPTION: add a reprocess channel that will do reprocess on frames
  *              coming from input channel
  *
  * PARAMETERS :
@@ -4485,7 +4485,7 @@ int32_t QCamera2HardwareInterface::addMetaDataChannel()
  *
  * RETURN     : Ptr to the newly created channel obj. NULL if failed.
  *==========================================================================*/
-QCameraReprocessChannel *QCamera2HardwareInterface::addOnlineReprocChannel(
+QCameraReprocessChannel *QCamera2HardwareInterface::addReprocChannel(
                                                       QCameraChannel *pInputChannel)
 {
     int32_t rc = NO_ERROR;
@@ -4627,6 +4627,7 @@ QCameraReprocessChannel *QCamera2HardwareInterface::addOnlineReprocChannel(
 
     ALOGD("%s: Allocating %d reproc buffers",__func__,minStreamBufNum);
 
+    bool offlineReproc = isRegularCapture();
     rc = pChannel->addReprocStreamsFromSource(*this,
                                               pp_config,
                                               pInputChannel,
@@ -4634,7 +4635,8 @@ QCameraReprocessChannel *QCamera2HardwareInterface::addOnlineReprocChannel(
                                               mParameters.getNumOfSnapshots(),
                                               &gCamCapability[mCameraId]->padding_info,
                                               mParameters,
-                                              mLongshotEnabled);
+                                              mLongshotEnabled,
+                                              offlineReproc);
     if (rc != NO_ERROR) {
         delete pChannel;
         return NULL;
@@ -6286,6 +6288,29 @@ int32_t QCamera2HardwareInterface::waitDefferedWork(int32_t &job_id)
 
     job_id = MAX_ONGOING_JOBS;
     return NO_ERROR;
+}
+
+/*===========================================================================
+ * FUNCTION   : isRegularCapture
+ *
+ * DESCRIPTION: Check configuration for regular catpure
+ *
+ * PARAMETERS :
+ *
+ * RETURN     : true - regular capture
+ *              false - other type of capture
+ *==========================================================================*/
+bool QCamera2HardwareInterface::isRegularCapture()
+{
+    bool ret = false;
+
+    if (numOfSnapshotsExpected() == 1 &&
+        !isLongshotEnabled() &&
+        !mParameters.getRecordingHintValue() &&
+        !isZSLMode()) {
+            ret = true;
+    }
+    return ret;
 }
 
 }; // namespace qcamera
