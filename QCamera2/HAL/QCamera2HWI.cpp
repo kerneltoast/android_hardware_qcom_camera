cf9ccc790925c7bea75c81b6013b69371c28caab
Gaoxiang Chen <gaochen@codeaurora.org>
2014-07-03 10:32:34 +0300

    QCamera2: Trigger ZSL snapshot only during flash in longshot
     Triggering ZSL snapshot during longshot could have
     side effects such as AEC locking in backend. This
     hook will only be enabled if flash is needed.
    
    Change-Id: I3ecee7a99653041c9eac3c1c81e14230973b4ce9

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -635,6 +635,7 @@ int QCamera2HardwareInterface::take_picture(struct camera_device *device)
             hw->waitAPIResult(QCAMERA_SM_EVT_PREPARE_SNAPSHOT, &apiResult);
             ret = apiResult.status;
         }
+        hw->mPrepSnapRun = true;
     }
 
     /* Regardless what the result value for prepare_snapshot,
@@ -1001,6 +1002,7 @@ QCamera2HardwareInterface::QCamera2HardwareInterface(int cameraId)
       mFlashNeeded(false),
       mCaptureRotation(0),
       mIs3ALocked(false),
+      mPrepSnapRun(false),
       mZoomLevel(0),
       m_bIntEvtPending(false),
       mSnapshotJob(-1),
@@ -2590,7 +2592,7 @@ int QCamera2HardwareInterface::takePicture()
                     return rc;
                 }
             }
-            if ( mLongshotEnabled ) {
+            if (mLongshotEnabled && mPrepSnapRun) {
                 mCameraHandle->ops->start_zsl_snapshot(
                         mCameraHandle->camera_handle,
                         pZSLChannel->getMyHandle());
@@ -3261,6 +3263,7 @@ int QCamera2HardwareInterface::sendCommand(int32_t command,
                 }
             }
             rc = mParameters.setLongshotEnable(mLongshotEnabled);
+            mPrepSnapRun = false;
         } else {
             rc = NO_INIT;
         }
@@ -3270,13 +3273,14 @@ int QCamera2HardwareInterface::sendCommand(int32_t command,
             cancelPicture();
             processEvt(QCAMERA_SM_EVT_SNAPSHOT_DONE, NULL);
             QCameraChannel *pZSLChannel = m_channels[QCAMERA_CH_TYPE_ZSL];
-            if (isZSLMode() && (NULL != pZSLChannel)) {
+            if (isZSLMode() && (NULL != pZSLChannel) && mPrepSnapRun) {
                 mCameraHandle->ops->stop_zsl_snapshot(
                         mCameraHandle->camera_handle,
                         pZSLChannel->getMyHandle());
             }
         }
         CDBG_HIGH("%s: Longshot Disabled", __func__);
+        mPrepSnapRun = false;
         mLongshotEnabled = false;
         rc = mParameters.setLongshotEnable(mLongshotEnabled);
         break;
