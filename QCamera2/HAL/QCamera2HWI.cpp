427c69ffd751ac68fa5ab0142217e54e8b12e1eb
Kerong Sui <ksui@codeaurora.org>
2014-08-19 13:29:51 +0800

    Camera: Fix HDR snapshot hang when Disable 1X frame
    Capture Channel is stopped unexpectedly during HDR snapshot
    after receive the 1st snapshot frame due to incorrect condition
    check in isRegularSnapshot()
    
    Change-Id: I11dc9b276af1624e8efeb45d9c00eab289fe2664

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -6567,7 +6567,7 @@ bool QCamera2HardwareInterface::isRegularCapture()
     if (numOfSnapshotsExpected() == 1 &&
         !isLongshotEnabled() &&
         !mParameters.getRecordingHintValue() &&
-        !isZSLMode()) {
+        !isZSLMode() && !mParameters.isHDREnabled()) {
             ret = true;
     }
     return ret;
